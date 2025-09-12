65b97d18c12f74cb4ddd618372d3c75b235d1fe7
saraNasser4 <saransser6@gmail.com>
2025-09-12 11:09:02 +0300

    [2] solve bug the server can't read .env file by install dotenv

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+import 'dotenv/config';
 import express from 'express';
 
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (res, req)=> {
-    res.sent('<h1>Hello World</h1>');
-    res.sent({ message: 'Hello World' });
+app.get('/', (req, res)=> {
+    res.send('<h1>Hello World</h1>');
+    //res.send({ message: 'Hello World' });
 })
 
 app.listen(PORT, ()=> console.log(`App running on port ${PORT}`))
