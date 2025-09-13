997cb99c6bebef77f4c84a47507f720746d6df7d
saraNasser4 <saransser6@gmail.com>
2025-09-13 15:24:40 +0300

    [4] GET using sendFile to access html pages

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
+import path from 'path';
+import url from 'url'
 
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.get('/', (req, res)=> {
-    res.send('<h1>Hello World</h1>');
+    //res.send('<h1>Hello World</h1>');
     //res.send({ message: 'Hello World' });
+
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
+
 app.get('/about', (req, res)=> {
-    res.send('<h2>About</h2>')
+    //res.send('<h2>About</h2>');
+
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
 })
 
 app.listen(PORT, ()=> console.log(`App running on port ${PORT}`))
