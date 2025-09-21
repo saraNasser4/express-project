2dc476f58d824d31cb759b2b2395dc9dce2853e4
saraNasser4 <saransser6@gmail.com>
2025-09-21 16:55:23 +0300

    [19] cleanup by using controllers

---
+++ controllers/postController.js
@@ -0,0 +1,80 @@
+let posts = [
+    { 'id': 1, 'title': 'Post 1' },
+    { 'id': 2, 'title': 'Post 2' },
+    { 'id': 3, 'title': 'Post 3' },
+];
+
+// @des    GET all posts
+// @route  GET /api/posts
+export const getAllPosts = (req, res)=> {
+    const limit = parseInt(req.query.limit);
+    if (!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+    res.status(200).json(posts);
+};
+
+//@des    GET a post
+//@route  GET /api/posts/:id
+export const getPost = (req, res, next)=> {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=> post.id === id);
+    
+    if (!post) {
+        const error = new Error(`Post Number ${id} Not Found`);
+        error.status = 404;
+        return next(error);
+    }
+    res.status(200).json(post);
+};
+
+// @des    POST a new post 
+// @route  POST /api/posts/
+export const createPost = (req, res, next)=> {
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body?.title || req.query.title,
+    }
+
+    if (!newPost.title) {
+        const err = new Error('Please include a title');
+        err.status = 400;
+        return next(err);
+    };
+
+    posts.push(newPost);
+
+    res.status(201).json(posts)
+};
+
+// @des   PUT an exist post
+// @route PUT /api/posts/:id
+export const updatePost = (req, res, next)=> {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+
+    if(!post) {
+        const err = new Error('Post not Found to be Updated');
+        err.status = 404;
+        return next(err);
+    } 
+
+    post.title = req.body?.title || req.query.title;
+
+    res.status(200).json(posts);
+};
+
+// @des    DELETE a post
+// @route  DELETE /api/posts/:id
+export const deletePost = (req, res, next)=> {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+
+    if(!post) {
+        const err = new Error('Post not Found');
+        err.status = 404;
+        return next(err);
+    } 
+    posts = posts.filter(post => post.id != id);
+    res.status(200).json(posts)
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,81 +1,23 @@
 import express from 'express';
+import { getAllPosts, getPost, createPost, updatePost, deletePost } from '../controllers/postController.js';
 
 const router = express.Router()
 
-let posts = [
-    { 'id': 1, 'title': 'Post 1' },
-    { 'id': 2, 'title': 'Post 2' },
-    { 'id': 3, 'title': 'Post 3' },
-];
+
 
 // http://localhost:8000/api/posts?limit=1
-router.get('/', (req, res)=> {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(posts.slice(0, limit));
-    }
-    res.status(200).json(posts);
-});
+router.get('/', getAllPosts);
 
 // get a single post
-router.get('/:id', (req, res, next)=> {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post)=> post.id === id);
-    
-    if (!post) {
-        const error = new Error(`Post Number ${id} Not Found`);
-        error.status = 404;
-        return next(error);
-    }
-    res.status(200).json(post);
-});
+router.get('/:id', getPost);
 
 // Create a new post
-router.post('/', (req, res, next)=> {
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body?.title || req.query.title,
-    }
-
-    if (!newPost.title) {
-        const err = new Error('Please include a title');
-        err.status = 400;
-        return next(err);
-    };
-
-    posts.push(newPost);
-
-    res.status(201).json(posts)
-});
+router.post('/', createPost);
 
 // PUT a post
-router.put('/:id', (req, res, next)=> {
-    const id = parseInt(req.params.id);
-    const post = posts.find(post => post.id === id);
-
-    if(!post) {
-        const err = new Error('Post not Found to be Updated');
-        err.status = 404;
-        return next(err);
-    } 
-
-    post.title = req.body?.title || req.query.title;
-
-    res.status(200).json(posts);
-});
+router.put('/:id', updatePost);
 
 // DELETE a post
-router.delete('/:id', (req, res, next)=> {
-    const id = parseInt(req.params.id);
-    const post = posts.find(post => post.id === id);
-
-    if(!post) {
-        const err = new Error('Post not Found');
-        err.status = 404;
-        return next(err);
-    } 
-    posts = posts.filter(post => post.id != id);
-    res.status(200).json(posts)
-})
+router.delete('/:id', deletePost)
 
 export default router;
